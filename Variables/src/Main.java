09ea269e08e464e15cee9734e82f6f0fa1b28a61
Suneel Manyam <hmanyam@pkrmsezdc.com>
2019-04-08 19:56:32 +0530

    variabule types

---
+++ Variables/src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int i =100;
+		int j = 50;
+		
+		byte b= 126;
+		
+		long l = 100L;
+		long l2 = 9000000000L;		
+		
+		System.out.println(i+j);
+		System.out.println(i-j);
+		System.out.println(i*5);
+		i+=5;
+		System.out.println(i);
+		int[] arr = {1,2,3,4};
+		System.out.println(arr[1]);
+		char[] myName = {'S','U','N','E','E','L'};
+		System.out.println(myName);
+		
+		
+
+	}
+
+}
